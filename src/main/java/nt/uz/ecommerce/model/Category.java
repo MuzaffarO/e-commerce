9ce1a605a4675fc39ae0830d853cc61f71962d99
MuzaffarO <muzaffarobidjonov0304@gmail.com>
2023-05-19 15:47:46 +0500

    deleted list of products from category

--- src/main/java/nt/uz/ecommerce/model/Category.java
+++ src/main/java/nt/uz/ecommerce/model/Category.java
@@ -14,7 +14,5 @@ public class Category {
     @SequenceGenerator(name = "category_id_seq", sequenceName = "category_id_seq", allocationSize = 1)
     private Integer id;
     private String name;
-    @OneToMany(mappedBy = "category")
-    private List<Products> products;
     private Integer parentId;
 }
